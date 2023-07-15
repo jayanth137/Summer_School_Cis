663a8eb4c490ee0643b7d56e11758bd7fd7df60d
Jayanth <jayanth.sms.in@gmail.com>
2023-07-16 05:10:22 +0530

    Fixed Bugs +1

--- src/component/CountdownSection/CountdownSection.jsx
+++ src/component/CountdownSection/CountdownSection.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React from 'react';
 
-import "./CountdownSection.css";
-import { motion } from "framer-motion";
-import Timer from "./Timer";
+import './CountdownSection.css';
+import { motion } from 'framer-motion';
+import Timer from './Timer';
 
 const CountdownSection = () => {
   return (
     <>
-      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { duration: 1 } }} viewport={{ once: false }}  className="event__section2" id="registration" >
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, transition: { duration: 1 } }}
+        viewport={{ once: false }}
+        className="event__section2"
+        id="registration"
+      >
         <div className="event_countdown_main">
           <div>
-            <h1>Registration Starting in </h1>
-            <hr style={{ marginBottom: "70px" }} />
+            <h1>Registration Closes in </h1>
+            <hr style={{ marginBottom: '70px' }} />
             {/* <h1>TBA</h1> */}
             <Timer />
           </div>
         </div>
       </motion.div>
-      <br/>
-
-      
+      <br />
     </>
   );
 };
